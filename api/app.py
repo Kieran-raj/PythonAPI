20e693fdff54fe9f2ec9975b1409aeaa62b6038c
Kieran <kieran@rajasansir.co.uk>
2021-05-23 19:12:37 +0100

    new files

--- api/app.py
+++ api/app.py
@@ -2,11 +2,19 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def home():
     if request.method == 'GET':
         return render_template('hello.html')
 
+
+@app.route('/add_data', methods=['GET', 'POST'])
+def enter_expense():
+    if request.method == 'GET':
+        return render_template('enter_data.html')
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
