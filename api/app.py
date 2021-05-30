6d4aafb1dc275d75a1d9c8b2b8d4eafe9b1e8e1f
Kieran Rajasansir <kieran@rajasansir.co.uk>
2021-05-30 21:38:16 +0100

    adding env file

--- api/app.py
+++ api/app.py
@@ -2,9 +2,10 @@ from flask import Flask
 from api.expenses.endpoints import bp
 
 
-def create_app():
+def create_app(config="api.config"):
     app = Flask(__name__)
 
+    app.config.from_object(config)
     # Registering Blueprints
     app.register_blueprint(bp)
 
