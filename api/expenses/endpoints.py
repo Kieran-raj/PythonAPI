4514819a030b27e4cf05f8896ee3674e154f8843
Kieran Rajasansir <kieran@rajasansir.co.uk>
2021-05-31 17:26:25 +0100

    got test database showing

--- api/expenses/endpoints.py
+++ api/expenses/endpoints.py
@@ -8,18 +8,22 @@ bp = Blueprint("expenses", __name__, url_prefix="/expenses")
 @bp.route('/', methods=['GET'])
 def home():
     if request.method == 'GET':
-        return render_template('welcome_page.html')
+        return render_template('welcome_page.html'), 200
 
 
-@bp.route('/data_base')
+@bp.route('/test_data', methods=['GET', 'POST'])
 def test():
-    result = db.engine.execute("select * from test_table;")
-    for i in result:
-        print(i)
-    return render_template('test_date.html', context=result)
+    if request.method == 'GET':
+        result = db.engine.execute("select * from test_table;")
+        return render_template('test_data.html', data=result), 200
+    elif request.method == 'POST':
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        print(first_name, last_name)
+        return {"message": "data collected"}, 200
 
 
 @bp.route('/enter_data', methods=['GET', 'POST'])
 def enter_expense():
     if request.method == 'GET':
-        return render_template('enter_data.html')
+        return render_template('enter_data.html'), 200
