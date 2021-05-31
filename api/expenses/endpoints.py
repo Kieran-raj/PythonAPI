341c8d763b0063e001ccb09d2d54d504fa54deb9
Kieran Rajasansir <kieran@rajasansir.co.uk>
2021-05-31 15:44:00 +0100

    connection to mysql

--- api/expenses/endpoints.py
+++ api/expenses/endpoints.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, current_app as app, request, render_template
+from api import db
 from api.templates import *
 
 bp = Blueprint("expenses", __name__, url_prefix="/expenses")
@@ -8,6 +9,14 @@ bp = Blueprint("expenses", __name__, url_prefix="/expenses")
 def home():
     if request.method == 'GET':
         return render_template('welcome_page.html')
+
+
+@bp.route('/data_base')
+def test():
+    result = db.engine.execute("show tables;")
+    for i in result:
+        print(i)
+    return {"message": 200}
 
 
 @bp.route('/enter_data', methods=['GET', 'POST'])
