96fb9f423db8426ecf5ca7813635a2145920293b
Kieran Rajasansir <kieran@rajasansir.co.uk>
2021-06-03 11:19:38 +0100

    add running total and data_functions folder

--- api/expenses/endpoints.py
+++ api/expenses/endpoints.py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 from datetime import date
-from flask import Blueprint, current_app as app, request, render_template
+from flask import Blueprint, current_app as app, request, render_template, url_for
 from api import db
 from api.templates import *
 
@@ -17,23 +17,30 @@ def enter_expense():
 
         if not data.empty:
             data['date'] = data['date'].dt.strftime('%Y-%m-%d')
-
-        return render_template('enter_data.html', data=json.loads(data.to_json(orient="records"))), 200
+            total = data['amount'].sum()
+            data_final = json.loads(data.to_json(orient="records"))
+            data_final[0]['total'] = total
+            return render_template('home_page.html', data=data_final), 200
+        else:
+            return render_template('home_page.html', data=json.loads(data.to_json(orient="records"))), 200
 
     elif request.method == 'POST':
-        description = request.form.get('description')
-        amount = request.form.get('amount')
-        category = request.form.get('category')
-        today = date.today().strftime("%Y-%m-%d")
+        if request.form['submit_button'] == 'Submit':
+            description = request.form.get('description')
+            amount = request.form.get('amount')
+            category = request.form.get('category').capitalize()
+            today = date.today().strftime("%Y-%m-%d")
 
-        insert_sql = f"""
-        INSERT INTO expenses_dev.expenses
-        (date, description, category, amount)
-        VALUES ('{today}', '{description}', '{category}', '{float(amount)}');
-        """
-        db.engine.execute(insert_sql)
-        data = pd.read_sql(
-            "SELECT * FROM expenses;", db.engine)
-
-        data['date'] = data['date'].dt.strftime('%Y-%m-%d')
-        return render_template('enter_data.html', data=json.loads(data.to_json(orient="records")))
+            insert_sql = f"""
+            INSERT INTO expenses_dev.expenses
+            (date, description, category, amount)
+            VALUES ('{today}', '{description}', '{category}', '{float(amount)}');
+            """
+            db.engine.execute(insert_sql)
+            data = pd.read_sql(
+                "SELECT * FROM expenses;", db.engine)
+            data['date'] = data['date'].dt.strftime('%Y-%m-%d')
+            total = data['amount'].sum()
+            data_final = json.loads(data.to_json(orient="records"))
+            data_final[0]['total'] = total
+            return render_template('home_page.html', data=data_final)
