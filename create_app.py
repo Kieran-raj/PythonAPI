9385d074e1a4c13b6b0510a250ded39e06f263ae
Kieran Rajasansir <kieran@rajasansir.co.uk>
2021-11-26 18:12:47 +0000

    added some todos and added testing database

--- create_app.py
+++ create_app.py
@@ -1,5 +1,9 @@
 from api.app import create_app
 
+# TODO: Allow for environment to be specified when running create app 
+# eg `python create_app.py --env developement`
+# if --env == development: config = api.config.DevConfig
+
 app = create_app('api.config.DevConfig')
 
 if __name__ == "__main__":
